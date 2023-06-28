607f28218d751279bc7e211835e133455903b934
soufi <47915535+soufied@users.noreply.github.com>
2023-06-29 02:54:40 +0300

    Add files via upload

---
+++ ExplodingCorpses/Configs/Config.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using Exiled.API.Interfaces;
+using PlayerRoles;
+namespace ExplodingCorpses.Configs
+{
+    public sealed class Config : IConfig
+    {
+        [Description("Is the plugin enabled?")]
+        public bool IsEnabled { get; set; } = true;
+        public bool Debug { get; set; } = false;
+        [Description("List of classes whose bodies will explode")]
+        public List<RoleTypeId> BoomClasses { get; set; } = new List<RoleTypeId>()
+        {
+            RoleTypeId.ClassD,
+            RoleTypeId.ChaosConscript,
+            RoleTypeId.ChaosMarauder,
+            RoleTypeId.ChaosRepressor,
+            RoleTypeId.ChaosRifleman,
+            RoleTypeId.NtfCaptain,
+            RoleTypeId.NtfPrivate,
+            RoleTypeId.NtfSergeant,
+            RoleTypeId.NtfSpecialist,
+            RoleTypeId.Scientist,
+            RoleTypeId.FacilityGuard,
+            RoleTypeId.Scp173,
+            RoleTypeId.Scp0492,
+            RoleTypeId.Scp049
+        };
+        [Description("How soon the grenade explodes")]
+        public float TickTack { get; set; } = 1f;
+        [Description("Damage Multiplier to SCP - (default - 3)")]
+        public float ScpDamageMultiplier { get; set; } = 3f;
+        [Description("Radius of explosion - (default - 9)")]
+        public float MaxRadius { get; set; } = 9f;
+    }
+}
---
+++ ExplodingCorpses/Plugin.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Exiled.API.Features;
+using Exiled.API.Features.Items;
+using Exiled.Events.EventArgs.Player;
+namespace ExplodingCorpses
+{
+    public class Plugin : Plugin<Configs.Config>
+    {
+        public override string Name => "ExplodingCorpses";
+        public override string Author => "_soufi";
+        public override Version Version => new Version(1, 0, 0);
+        public override Version RequiredExiledVersion => new Version(7, 1, 0);
+        public static Plugin Singleton = new Plugin();
+        public override void OnEnabled()
+        {
+            Singleton = this;
+            Exiled.Events.Handlers.Player.SpawningRagdoll += OnSpawningRagdoll;
+            base.OnEnabled();
+        }
+        public override void OnDisabled()
+        {
+            Singleton = null;
+            Exiled.Events.Handlers.Player.SpawningRagdoll -= OnSpawningRagdoll;
+            base.OnDisabled();
+        }
+        public void OnSpawningRagdoll(SpawningRagdollEventArgs ev)
+        {
+            if (!Config.BoomClasses.Contains(ev.Role)) return;
+            var grenade = (ExplosiveGrenade)Item.Create(ItemType.GrenadeHE);
+            grenade.FuseTime = Config.TickTack;
+            grenade.MaxRadius = Config.MaxRadius;
+            grenade.ScpDamageMultiplier = Config.ScpDamageMultiplier;
+            grenade.SpawnActive(ev.Position);
+        }
+    }
+}
